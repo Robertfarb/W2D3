e37e5672efaab2dbee25748f5bf6b9861716d71e
robertfarb <farb.robert@gmail.com>
2018-06-27 15:16:13 -0700

    Finish towers of hanoi tdd

--- towers_of_hanoi_tdd/lib/board.rb
+++ towers_of_hanoi_tdd/lib/board.rb
@@ -12,14 +12,18 @@ class Board
   end
   
   def valid_move?(from_tower, to_tower)
+    return false unless from_tower.between?(0, 2) || to_tower.between?(0, 2)
     return false if towers[from_tower].empty?
     return true if towers[to_tower].empty?
     towers[from_tower].last < towers[to_tower].last
   end
+  
+  def won?
+    return true if towers[1] == [3, 2, 1] || towers[2] == [3, 2, 1]
+    false
+  end
+  
+  def render
+    p towers
+  end
 end
-
-#valid_move?
-#move
-#won?
-#initialize
-#render
--- towers_of_hanoi_tdd/lib/game.rb
+++ towers_of_hanoi_tdd/lib/game.rb
@@ -1,6 +1,32 @@
+require_relative 'board'
+
 class Game
+  attr_reader :board, :player
+  
+  def initialize(player = 'Bob', board)
+    @player = player
+    @board = board
+  end
+  
+  def play
+    until board.won?
+      board.render
+      move = get_move
+      board.move(move.first, move.last)
+    end
+  end
+  
+  def get_move
+    puts "Please choose a move (Ex. 2, 1)"
+    move = gets.chomp
+    move_arr = move.split(", ").map(&:to_i)
+    return move_arr
+  end
 end
 
-#play
-#get_move
-#win_message
+
+if __FILE__ ==  $PROGRAM_NAME
+  board = Board.new
+  game = Game.new("Garbo", board)
+  game.play
+end 
